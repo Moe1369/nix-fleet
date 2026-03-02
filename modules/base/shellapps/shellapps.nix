8fe8a256152f7e312c63190ecdb5fb1897c5289a
Mohamed C. <mohamed@chrayed.de>
2026-03-03 00:35:06 +0100

    shellapps

---
+++ modules/base/shellapps/shellapps.nix
@@ -0,0 +1,10 @@
+{ ... }: {
+  flake.nixosModules.shellapps = { pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+      wget
+      git
+      nil
+      yaml-language-server
+    ];
+  };
+}
