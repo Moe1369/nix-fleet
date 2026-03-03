11fa6d5acd50bb7995e795484d1c03ada4aea134
Mohamed Chrayed <mo@chrayed.de>
2026-03-03 16:03:07 +0100

    sops debugging

--- modules/base/users/user.nix
+++ modules/base/users/user.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.user = { pkgs, config, user, fullname, ... }: {
     home-manager.extraSpecialArgs = { inherit user fullname; };
 
-    sops.secrets."users/${user}" = {
+    sops.secrets."users/${user}/password" = {
       neededForUsers = true;
     };
     
