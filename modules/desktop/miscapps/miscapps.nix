63b670c7b59e5fc9902d9151e6066b666b020c6f
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-03 21:13:50 +0100

    duplicate

--- modules/desktop/miscapps/miscapps.nix
+++ modules/desktop/miscapps/miscapps.nix
@@ -2,8 +2,6 @@
   flake.nixosModules.miscapps = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
       vesktop
-      ibm-plex
-      adwaita-fonts
       pciutils
       aha
     ];
