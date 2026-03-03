0a1246e5c6de6034999d70962b432e72e26a8470
Mohamed Chrayed <mo@chrayed.de>
2026-03-03 14:03:05 +0100

    add lanzaboote

--- modules/security/secure-boot/secure-boot.nix
+++ modules/security/secure-boot/secure-boot.nix
@@ -1,7 +1,16 @@
-{ ... }: {
-  flake.nixosModules.secure-boot = { pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-      sbctl
+{ inputs, ... }: {
+  flake.nixosModules.secure-boot = { pkgs, lib, ... }: {
+    imports = [
+      inputs.lanzaboote.nixosModules.lanzaboote
     ];
+
+    environment.systemPackages = [ pkgs.sbctl ];
+
+    boot.loader.systemd-boot.enable = lib.mkForce false;
+
+    boot.lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+    };
   };
 }
