a733b4dd6072eb9cbe5a7064dabf407164b1f6f4
Mohamed C. <mohamed@chrayed.de>
2026-03-03 10:17:02 +0100

    add specialargs

--- modules/hosts/computer-mo/nixos-configurations.nix
+++ modules/hosts/computer-mo/nixos-configurations.nix
@@ -1,6 +1,7 @@
 { inputs, ... }: {
   flake.nixosConfigurations.computer-mo = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
+    specialArgs = { user = "mo"; };
     modules = [
       inputs.self.nixosModules.computer-mo
     ];
