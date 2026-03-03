c261469d0736d904832e5584f5afc400bd1aff91
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-03 23:42:42 +0100

    binding

--- modules/gnome/usr/gnome-mo.nix
+++ modules/gnome/usr/gnome-mo.nix
@@ -24,6 +24,7 @@
       };
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         command = ["kgx --tab"];
+        binding = ["<Super>Return"];
         name = ["Console"];
       };
       "org/gnome/desktop/wm/keybindings" = {
