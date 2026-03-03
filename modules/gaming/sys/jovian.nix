e7f8a72d5cb1ec28ad55ee26c8ef4f1811eb33a7
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-03 23:09:21 +0100

    error

--- modules/gaming/sys/jovian.nix
+++ modules/gaming/sys/jovian.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.gaming-sys-jovian = { ... }:{
     jovian.steam.enable = true;
     jovian.steam.autoStart = true;
-    jovian.steam.user = deck;
+    jovian.steam.user = "deck";
     jovian.steam.desktopSession = "gnome";
   };
 }
