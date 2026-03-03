1fdb60b5bb23bf5a1b7a06f3beba038d0533073b
Mohamed C. <mohamed@chrayed.de>
2026-03-03 10:20:05 +0100

    pkgs-unstable

--- modules/hosts/computer-mo/nixos-configurations.nix
+++ modules/hosts/computer-mo/nixos-configurations.nix
@@ -1,7 +1,13 @@
 { inputs, ... }: {
   flake.nixosConfigurations.computer-mo = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    specialArgs = { user = "mo"; };
+    specialArgs = {
+      user = "mo";
+      pkgs-unstable = import inputs.nixpkgs-unstable {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    };
     modules = [
       inputs.self.nixosModules.computer-mo
     ];
