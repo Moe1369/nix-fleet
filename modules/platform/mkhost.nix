ea4412e8c1288a2011b15ae40b43f068dcc77b87
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-03 20:47:16 +0100

    debug

--- modules/platform/mkhost.nix
+++ modules/platform/mkhost.nix
@@ -1,17 +1,9 @@
 { inputs, ... }: {
-  flake.lib.mkHost = { system, user, host, extraModules ? [] }:
-    let
-      fullnames = {
-        mo            = "Mohamed Chrayed";
-        nadine        = "Nadine Peukert";
-        administrator = "Administrator";
-      };
-    in
+  flake.lib.mkHost = { system, host, extraModules ? [] }:
     inputs.nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = {
-        inherit user host system;
-        fullname = fullnames.${user};
+        inherit host system;
         pkgs-unstable = import inputs.nixpkgs-unstable {
           inherit system;
           config.allowUnfree = true;
