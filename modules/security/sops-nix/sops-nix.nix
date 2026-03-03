035f3be30194881e4292c6e4d5f77469cdf3771c
Mohamed Chrayed <mo@chrayed.de>
2026-03-03 15:55:36 +0100

    sops debugging

--- modules/security/sops-nix/sops-nix.nix
+++ modules/security/sops-nix/sops-nix.nix
@@ -1,9 +1,9 @@
 { inputs, ... }: {
   flake.nixosModules.sops-nix = { pkgs, ... }: {
-    imports = [
-      inputs.sops-nix.nixosModules.sops
-    ];
+    imports = [ inputs.sops-nix.nixosModules.sops ];
 
     environment.systemPackages = [ pkgs.age pkgs.sops ];
+
+    sops.defaultSopsFile = ./secrets/secrets.yaml;
   };
 }
