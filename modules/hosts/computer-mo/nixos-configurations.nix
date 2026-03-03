7c4652f8a0968452c793279771fdc4d18ac9e60b
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-03 20:45:45 +0100

    remove user

--- modules/hosts/computer-mo/nixos-configurations.nix
+++ modules/hosts/computer-mo/nixos-configurations.nix
@@ -1,14 +1,14 @@
 { inputs, ... }:
 let
   system = "x86_64-linux";
-  user   = "mo";
+#  user   = "mo";
   host   = "computer-mo";
 
   mkHost = inputs.self.lib.mkHost;
 in
 {
   flake.nixosConfigurations.${host} = mkHost {
-    inherit system user host;
+    inherit system host;
     extraModules = [ inputs.self.nixosModules.${host} ];
   };
 }
