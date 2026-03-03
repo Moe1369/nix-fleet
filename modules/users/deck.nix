c07eb12c352695f4cc023b5ae1cd83ad82d0e8bd
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-03 23:08:15 +0100

    deck user

---
+++ modules/users/deck.nix
@@ -0,0 +1,23 @@
+{ inputs, ... }: {
+  flake.nixosModules.mo = { pkgs, config, ... }: {
+    home-manager.extraSpecialArgs = {};
+
+    sops.secrets."users/deck/password" = {
+      neededForUsers = true;
+    };
+    users.mutableUsers = false;     
+    users.users.mo = {
+      isNormalUser = true;
+      description = "SteamDeck";
+      extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+      shell = pkgs.zsh;
+      hashedPasswordFile = config.sops.secrets."users/deck/password".path;
+    };
+
+    home-manager.users.mo = {
+      home.username = "deck";
+      home.homeDirectory = "/home/deck";
+      home.stateVersion = "25.11";
+    };
+  };
+}
