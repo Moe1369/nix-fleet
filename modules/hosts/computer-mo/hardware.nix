139b9129916642e05b0627a9e197241d9cb596c1
Mohamed C. <mohamed@chrayed.de>
2026-03-03 11:37:37 +0100

    system variable

--- modules/hosts/computer-mo/hardware.nix
+++ modules/hosts/computer-mo/hardware.nix
@@ -1,8 +1,8 @@
-{
-  flake.nixosModules.computer-mo = {
-    boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-    boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"  ];
-    nixpkgs.hostPlatform = "x86_64-linux";
+{ ... }: {
+  flake.nixosModules.computer-mo = { system, ... }: {
+    boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" "nvme" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
+    nixpkgs.hostPlatform = system;
     hardware.cpu.amd.updateMicrocode = true;
     boot.kernelModules = [ "kvm-amd" ];
   };
