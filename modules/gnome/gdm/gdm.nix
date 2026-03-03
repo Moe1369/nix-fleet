5db36a4e8a3bce088074bf22c9cd7fd624a52c7e
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-03 20:54:30 +0100

    debug

--- modules/gnome/gdm/gdm.nix
+++ modules/gnome/gdm/gdm.nix
@@ -1,7 +1,7 @@
 { ... }: {
   flake.nixosModules.gdm = { ... }:{
     services.displayManager.gdm.enable = true;
-    services.displayManager.autoLogin.enable = true;
+    #services.displayManager.autoLogin.enable = true;
     #services.displayManager.autoLogin.user = user;
   };
 }
