db454e050c001748e7bb959803e47815ab00aa8d
Mohamed Chrayed <mo@chrayed.de>
2026-03-03 20:44:22 +0100

    debug

--- modules/base/git/git.nix
+++ modules/base/git/git.nix
@@ -1,9 +1,9 @@
 { inputs, ... }: {
-  flake.homeModules.git = { user, fullname, ... }: {
+  flake.homeModules.git = { ... }: {
     programs.git = {
       enable = true;
-      settings.user.name = fullname;
-      settings.user.email = "${user}@chrayed.de";
+      settings.user.name = "Mohamed Chrayed";
+      settings.user.email = "mohamed@chrayed.de";
       settings = {
         init.defaultBranch = "main";
         core.editor = "nano";
