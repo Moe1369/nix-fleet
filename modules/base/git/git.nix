7561e6b1b5458650e0339158f26a17854536fdae
Mohamed Chrayed <mo@chrayed.de>
2026-03-03 13:52:06 +0100

    renamed git settings

--- modules/base/git/git.nix
+++ modules/base/git/git.nix
@@ -2,9 +2,9 @@
   flake.homeModules.git = { user, fullname, ... }: {
     programs.git = {
       enable = true;
-      settings.userName = fullname;
-      settings.userEmail = "${user}@chrayed.de";
-      extraConfig = {
+      settings.user.name = fullname;
+      settings.user.email = "${user}@chrayed.de";
+      settings = {
         init.defaultBranch = "main";
         core.editor = "nano";
       };
