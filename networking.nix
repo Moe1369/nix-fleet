2606b75ef17cffb62824b73ced7dde7171aeecee
Mohamed C. <mohamed@chrayed.de>
2026-03-03 01:23:45 +0100

    wrong location

--- networking.nix
+++
@@ -1,7 +0,0 @@
-{ ... }: {
-  flake.nixosModules.network = {lib, hostName, ... }:{
-    networking.firewall.enable = true;
-    networking.useDHCP = true;
-    networking.networkmanager.enable = true;
-  };
-}
