08acf605125690f2e698716a31ae581e3a2e4977
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 19:40:38 +0100

    debug luks auto tpm

--- modules/base/sys/boot.nix
+++ modules/base/sys/boot.nix
@@ -1,15 +1,15 @@
 { ... }: {
-  flake.nixosModules.base-sys-boot = {
+  flake.nixosModules.base-sys-boot = {lib, ... }:{
     boot.loader.systemd-boot.enable = true;
     boot.loader.systemd-boot.consoleMode = "max";
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.timeout = 1;
-    boot.consoleLogLevel = 5;
+    boot.consoleLogLevel = 0;
     boot.initrd.verbose = false;
     boot.initrd.enable = true;
     boot.initrd.systemd.enable = true;
-    boot.plymouth.enable = false;
-    #boot.plymouth.theme = "bgrt";
+    boot.plymouth.enable = true;
+    boot.plymouth.theme = "bgrt";
     boot.initrd.luks.devices."cryptroot" = {
     device = lib.mkForce "/dev/nvme0n1p2";
       #crypttabExtraOpts = [ "tpm2-device=auto" ];
