1c713e8189d465d23fd3896aa1070be79e4422b1
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 14:54:13 +0100

    reorder secrets

--- modules/base/usr/ssh-all.nix
+++ modules/base/usr/ssh-all.nix
@@ -1,9 +1,9 @@
 { config, ... }: {
   flake.homeModules.base-usr-ssh-all = { config, ... }: {
-    sops.secrets."users/mo/intern/public" = {
+    sops.secrets."ssh/intern/public" = {
       path = "${config.home.homeDirectory}/.ssh/intern.pub";
     };
-    sops.secrets."users/mo/extern/private" = {
+    sops.secrets."ssh/extern/private" = {
       path = "${config.home.homeDirectory}/.ssh/extern";
     };
   };
