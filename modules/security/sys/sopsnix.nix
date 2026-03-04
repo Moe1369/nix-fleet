f7fac14f7142edbd6154ffbe2d2a6cd12da30b22
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 14:14:59 +0100

    sops debugging

--- modules/security/sys/sopsnix.nix
+++ modules/security/sys/sopsnix.nix
@@ -1,7 +1,6 @@
 { inputs, ... }: {
   flake.nixosModules.security-sys-sopsnix = { config, pkgs, ... }: {
     imports = [ inputs.sops-nix.nixosModules.sops ];
-    sops.age.keyFile = "/home/mo/.config/sops/age/keys.txt";
     environment.systemPackages = [ pkgs.age pkgs.sops ];
     sops.defaultSopsFile = ../secrets/secrets.yaml;
   };
--- modules/security/usr/sopsnix.nix
+++ modules/security/usr/sopsnix.nix
@@ -1,6 +1,7 @@
 { inputs, ... }: {
   flake.homeModules.security-usr-sopsnix = { config, pkgs, ... }: {
     imports = [ inputs.sops-nix.homeManagerModules.sops ];
+    sops.age.keyFile = "/home/mo/.config/sops/age/keys.txt";
     sops.defaultSopsFile = ../secrets/secrets.yaml;
   };
 }
