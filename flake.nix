02a104136923a01066f13d9df7ca9867f2c26459
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 18:07:41 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
   
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; }
