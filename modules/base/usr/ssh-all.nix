9acbfe9868b05bc394a0e734b740138d2dbeec0a
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 14:10:15 +0100

    sops debugging

--- modules/base/usr/ssh-all.nix
+++ modules/base/usr/ssh-all.nix
@@ -1,9 +1,9 @@
 { ... }: {
   flake.homeModules.base-usr-ssh-all = { config, ... }: {
-  #  sops.secrets."users.mo.intern.public" = {};
-  #  sops.secrets."users.mo.extern.private" = {};
+    sops.secrets."users.mo.intern.public" = {};
+    sops.secrets."users.mo.extern.private" = {};
 
-   # home.file.".intern.pub".source = config.sops.secrets."users.mo.intern.public".path;
-   # home.file.".extern".source = config.sops.secrets."users.mo.extern.private".path;
+    home.file.".intern.pub".source = config.sops.secrets."users.mo.intern.public".path;
+    home.file.".extern".source = config.sops.secrets."users.mo.extern.private".path;
   };
 }
