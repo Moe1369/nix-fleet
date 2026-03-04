15e9bc235e1723655f1540de8c2c3e5900fd076f
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 14:55:47 +0100

    reorder secrets

--- modules/base/usr/ssh-all.nix
+++ modules/base/usr/ssh-all.nix
@@ -3,6 +3,12 @@
     sops.secrets."ssh/intern/public" = {
       path = "${config.home.homeDirectory}/.ssh/intern.pub";
     };
+    sops.secrets."ssh/intern/private" = {
+      path = "${config.home.homeDirectory}/.ssh/intern";
+    };
+    sops.secrets."ssh/extern/public" = {
+      path = "${config.home.homeDirectory}/.ssh/extern.pub";
+    };
     sops.secrets."ssh/extern/private" = {
       path = "${config.home.homeDirectory}/.ssh/extern";
     };
