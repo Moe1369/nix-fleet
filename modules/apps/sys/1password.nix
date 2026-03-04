0e2f7e4feeca9ec2e8d9ece94c30842fec0a33c6
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 12:22:01 +0100

    1password polkit

--- modules/apps/sys/1password.nix
+++ modules/apps/sys/1password.nix
@@ -2,5 +2,6 @@
   flake.nixosModules.apps-sys-onepassword = {
     programs._1password.enable = true;
     programs._1password-gui.enable = true;
+    programs._1password-gui.polkitPolicyOwners = [ "mo" "deck" ];
   };
 }
