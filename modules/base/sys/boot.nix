076b21777f27253c693df10456ceb0b26bc8fb37
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-04 19:29:47 +0100

    debug luks auto tpm

--- modules/base/sys/boot.nix
+++ modules/base/sys/boot.nix
@@ -4,15 +4,15 @@
     boot.loader.systemd-boot.consoleMode = "max";
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.timeout = 1;
-    boot.consoleLogLevel = 0;
+    boot.consoleLogLevel = 5;
     boot.initrd.verbose = false;
     boot.initrd.enable = true;
     boot.initrd.systemd.enable = true;
-    boot.plymouth.enable = true;
-    boot.plymouth.theme = "bgrt";
-#    boot.initrd.luks.devices."cryptroot" = {
-      #device = "/dev/disk/by-label/luks";
-#      crypttabExtraOpts = [ "tpm2-device=auto" ];
-#    };
+    boot.plymouth.enable = false;
+    #boot.plymouth.theme = "bgrt";
+    #boot.initrd.luks.devices."cryptroot" = {
+    #device = "/dev/disk/by-label/luks";
+      #crypttabExtraOpts = [ "tpm2-device=auto" ];
+    #};
   };
 }
