2eb9f4d16d3ef1944bc9a103109cffb30aac22a3
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-05 17:53:21 +0100

    steam env

--- modules/gaming/sys/steam.nix
+++ modules/gaming/sys/steam.nix
@@ -7,8 +7,6 @@
         extraEnv = {
           GAMEMODERUN = "1";
           PROTON_ENABLE_HDR = "1";
-          DXVK_HDR = "1";
-          ENABLE_HDR_WSI = "1";
           PROTON_FSR4_UPGRADE = "1";
           DXIL_SPIRV_CONFIG = "wmma_rdna3_workaround"; 
          };
