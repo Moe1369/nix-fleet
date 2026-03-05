6c93fdcc413a02da017ac6640dcb5bb68c4eca0d
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-05 14:57:55 +0100

    nexusmods

--- modules/gaming/sys/nexusmods.nix
+++ modules/gaming/sys/nexusmods.nix
@@ -1,5 +1,9 @@
 { ... }: {
   flake.nixosModules.gaming-sys-nexusmods = { pkgs, ... }: {
+    nixpkgs.config.permittedInsecurePackages = [
+      "nexusmods-app-unfree-0.21.1"
+    ];
+
     environment.systemPackages = with pkgs; [
       nexusmods-app-unfree
     ];
