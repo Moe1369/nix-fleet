343c34ebe445dd3a263642d517c4a3d7586db9eb
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-05 14:55:55 +0100

    nexusmods

--- modules/gaming/sys/nexusmods.nix
+++ modules/gaming/sys/nexusmods.nix
@@ -1,10 +1,7 @@
 { ... }: {
   flake.nixosModules.gaming-sys-nexusmods = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
-      nexusmods-app
-      (pkgs.nexusmods-app.override {
-        _7zz = pkgs._7zz-rar;
-      })
+      nexusmods-app-unfree
     ];
   };
 }
