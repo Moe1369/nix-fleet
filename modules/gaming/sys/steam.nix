2720122fe65097031f694878afc77253675262a5
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-05 17:57:09 +0100

    steam env

--- modules/gaming/sys/steam.nix
+++ modules/gaming/sys/steam.nix
@@ -6,6 +6,7 @@
       package = pkgs-unstable.steam.override {
         extraEnv = {
           GAMEMODERUN = "1";
+          PROTON_ENABLE_WAYLAND = "1";
           PROTON_ENABLE_HDR = "1";
           PROTON_FSR4_UPGRADE = "1";
           DXIL_SPIRV_CONFIG = "wmma_rdna3_workaround"; 
