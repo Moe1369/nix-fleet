67ed87de5e07bed878fbbdcd95dbeb3479824423
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-06 18:22:17 +0100

    no gnupg keys

--- modules/security/sys/sopsnix.nix
+++ modules/security/sys/sopsnix.nix
@@ -4,6 +4,7 @@
     environment.systemPackages = [ pkgs.age pkgs.sops ];
     sops.defaultSopsFile = ../secrets/secrets.yaml;
     sops.age.sshKeyPaths = [];
+    sops.gnupg.sshKeyPaths = [];
     sops.age.keyFile = "/var/lib/sops/root-keys/master-host.txt";
     systemd.tmpfiles.rules = [
       "d /var/lib/sops/root-keys 0700 root root -"
