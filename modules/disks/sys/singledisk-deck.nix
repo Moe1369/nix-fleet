3712ff77f11c250167d8d1b62371f461bc8e6a48
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-06 19:25:57 +0100

    prepare deck

---
+++ modules/disks/sys/singledisk-deck.nix
@@ -0,0 +1,44 @@
+{ ... }: {
+  flake.nixosModules.disks-sys-singledisk-deck = { inputs, ... }: {
+    imports = [ inputs.disko.nixosModules.disko ];
+
+    disko.devices = {
+      disk = {
+        main = {
+          type = "disk";
+          device = "/dev/nvme0n1";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "fmask=0077" "dmask=0077" ];
+                  extraArgs = [ "-n" "boot" ];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "xfs";
+                  mountpoint = "/";
+                  extraArgs = [ "-L" "root" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    swapDevices = [{
+      device = "/var/lib/swapfile";
+      size = 16 * 1024;
+    }];
+  };
+}
