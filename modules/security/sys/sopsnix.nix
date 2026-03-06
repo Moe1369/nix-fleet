23d4092301d146927abc080e79433c063e1e6c9e
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-06 18:18:39 +0100

    dont use ssh

--- modules/security/sys/sopsnix.nix
+++ modules/security/sys/sopsnix.nix
@@ -3,6 +3,7 @@
     imports = [ inputs.sops-nix.nixosModules.sops ];
     environment.systemPackages = [ pkgs.age pkgs.sops ];
     sops.defaultSopsFile = ../secrets/secrets.yaml;
+    sops.age.sshKeyPaths = [];
     sops.age.keyFile = "/var/lib/sops/root-keys/master-host.txt";
     systemd.tmpfiles.rules = [
       "d /var/lib/sops/root-keys 0700 root root -"
