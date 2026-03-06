96a58ec0539c21678abd04eb36b7d31fc4de5a42
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-06 18:55:42 +0100

    new key location

--- modules/security/usr/sopsnix.nix
+++ modules/security/usr/sopsnix.nix
@@ -1,7 +1,8 @@
 { inputs, ... }: {
   flake.homeModules.security-usr-sopsnix = { config, pkgs, ... }: {
     imports = [ inputs.sops-nix.homeManagerModules.sops ];
-    sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+#    sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+    sops.age.keyFile = "/var/lib/sops/user-keys/master-user.txt";
     sops.defaultSopsFile = ../secrets/secrets.yaml;
   };
 }
