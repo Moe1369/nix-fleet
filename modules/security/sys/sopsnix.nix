f4d432e3a1c1c9d6d148ee83b283fef682061316
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-06 18:14:04 +0100

    new filename

--- modules/security/sys/sopsnix.nix
+++ modules/security/sys/sopsnix.nix
@@ -3,7 +3,7 @@
     imports = [ inputs.sops-nix.nixosModules.sops ];
     environment.systemPackages = [ pkgs.age pkgs.sops ];
     sops.defaultSopsFile = ../secrets/secrets.yaml;
-    sops.age.keyFile = "/var/lib/sops/root/keys.txt";
+    sops.age.keyFile = "/var/lib/sops/root/master-host.txt";
     systemd.tmpfiles.rules = [
       "d /var/lib/sops/root-keys 0700 root root -"
      ];
