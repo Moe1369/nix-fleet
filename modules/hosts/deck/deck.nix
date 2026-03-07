fa322f10dc0e780571d8d70d461faae43eed04cb
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-07 15:45:54 +0100

    deck debugging

--- modules/hosts/deck/deck.nix
+++ modules/hosts/deck/deck.nix
@@ -27,6 +27,7 @@
 
     boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "ahci" "usbhid" "usb_storage" "sd_mod" ];
     boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
+    boot.initrd.kernelModules = [ "nvme" ];
     nixpkgs.hostPlatform = system;
     hardware.cpu.amd.updateMicrocode = true;
   };
