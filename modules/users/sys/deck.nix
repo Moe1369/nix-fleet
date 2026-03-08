0265d27e1336caf1aeafb9a06711c0d91c6e08aa
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-08 19:19:03 +0100

    settings

--- modules/users/sys/deck.nix
+++ modules/users/sys/deck.nix
@@ -1,19 +1,27 @@
 { inputs, ... }: {
   flake.nixosModules.users-sys-deck = { pkgs, config, ... }: {
-    home-manager.extraSpecialArgs = {};
-
+  
     sops.secrets."users/deck/password" = {
       neededForUsers = true;
     };
+    sops.secrets."ssh/intern/public" = {
+      owner = "deck";
+      path = "/home/deck/.ssh/authorized_keys";
+      mode = "0600";
+    };
+    
+    systemd.tmpfiles.rules = [
+      "d /home/deck/.ssh 0700 deck users -"
+    ];
+
     users.mutableUsers = false;     
     users.users.deck = {
       isNormalUser = true;
-      description = "SteamDeck";
+      description = "Deck";
       extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
       shell = pkgs.zsh;
       hashedPasswordFile = config.sops.secrets."users/deck/password".path;
     };
-
     home-manager.users.deck = {
       home.username = "deck";
       home.homeDirectory = "/home/deck";
--- modules/users/sys/nadine.nix
+++ modules/users/sys/nadine.nix
@@ -4,6 +4,16 @@
     sops.secrets."users/nadine/password" = {
       neededForUsers = true;
     };
+    sops.secrets."ssh/intern/public" = {
+      owner = "nadine";
+      path = "/home/nadine/.ssh/authorized_keys";
+      mode = "0600";
+    };
+    
+    systemd.tmpfiles.rules = [
+      "d /home/nadine/.ssh 0700 nadine users -"
+    ];
+
     users.mutableUsers = false;     
     users.users.nadine = {
       isNormalUser = true;
@@ -12,7 +22,6 @@
       shell = pkgs.zsh;
       hashedPasswordFile = config.sops.secrets."users/nadine/password".path;
     };
-
     home-manager.users.nadine = {
       home.username = "nadine";
       home.homeDirectory = "/home/nadine";
