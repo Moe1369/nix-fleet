e8281500f3d5152b5fc136d953186333fb9eb09f
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-10 00:17:03 +0100

    jovian tweaks

--- modules/gaming/sys/jovian.nix
+++ modules/gaming/sys/jovian.nix
@@ -2,6 +2,10 @@
   flake.nixosModules.gaming-sys-jovian = { inputs, ... }:{
     imports = [ inputs.jovian-nixos.nixosModules.default ];
     jovian.steam.enable = true;
+    jovian.decky-loader.enable = true;
+    jovian.decky-loader.stateDir = "/var/lib/decky-loader";
+    jovian.decky-loader.user = "decky";
+    jovian.devices.steamdeck.autoUpdate = true;
     jovian.steam.autoStart = true;
     jovian.devices.steamdeck.enable = true; 
     jovian.steam.user = "deck";
