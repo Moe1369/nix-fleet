5a57ea9ee01dbeb38a09881112a7be3cceaacc6c
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-10 00:07:19 +0100

    steamdeck hardware

--- modules/gaming/sys/jovian.nix
+++ modules/gaming/sys/jovian.nix
@@ -3,6 +3,7 @@
     imports = [ inputs.jovian-nixos.nixosModules.default ];
     jovian.steam.enable = true;
     jovian.steam.autoStart = true;
+    jovian.devices.steamdeck.enable = true; 
     jovian.steam.user = "deck";
     jovian.steam.desktopSession = "plasma";
     services.displayManager.sddm.wayland.enable = true;
