d8c3db3c80094c38d5681e4217fcbf44841e13a1
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-11 11:07:23 +0100

    cache debugging

--- modules/platform/cache.nix
+++ modules/platform/cache.nix
@@ -1,10 +1,16 @@
 { ... }: {
   flake.nixosModules.platform-sys-cache = { config, ... }: {
-    sops.secrets."garnix/token" = {};
+    sops.secrets."garnix/token" = {
+      path = "/etc/nix/netrc";
+      mode = "0400";
+    };
     nix.settings = {
       substituters = [ "https://cache.garnix.io" ];
       trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
-      netrc-file = config.sops.secrets."garnix/token".path;
+      netrc-file = "/etc/nix/netrc";
     };
+    nix.extraOptions = ''
+      narinfo-cache-positive-ttl = 3600
+    '';
   };
 }
