e8a6cadde368e978a3dadeee1b3ca72bd54e1e12
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-11 20:34:51 +0100

    vesktop silent

--- modules/apps/usr/vesktop.nix
+++ modules/apps/usr/vesktop.nix
@@ -6,7 +6,7 @@
         Type=Application
         Name=Vesktop
         Comment=Vesktop autostart script
-        Exec=${pkgs.vesktop}/bin/vesktop --enable-speech-dispatcher -silent
+        Exec=${pkgs.vesktop}/bin/vesktop --enable-speech-dispatcher --silent
         StartupNotify=false
         Terminal=false
         Icon=vesktop
