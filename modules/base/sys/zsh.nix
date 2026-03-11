e4b8c17439c7be5c9c78e0d3a719f17e64a46837
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-11 12:34:28 +0100

    alias

--- modules/base/sys/zsh.nix
+++ modules/base/sys/zsh.nix
@@ -9,8 +9,8 @@
     programs.zsh.ohMyZsh.theme = "agnoster";
     users.defaultUserShell = pkgs.zsh;
     programs.zsh.shellAliases = {
-      update = "sudo nix flake update --flake git+https://gitea.chrayed.de/Chrayed/nix-fleet";
-      rebuild = "sudo nixos-rebuild switch --refresh --flake git+https://gitea.chrayed.de/Chrayed/nix-fleet#${host}";
+      rebuild-from-gitea = "sudo nixos-rebuild switch --refresh --flake git+https://gitea.chrayed.de/Chrayed/nix-fleet#${host}";
+      rebuild = "sudo nixos-rebuild switch --refresh --flake github:Moe1369/nix-fleet#${host}";
     };
   };
 }
