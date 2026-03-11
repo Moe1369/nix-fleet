e7ea55716d2c1433bbb6011ea70479d820a1a631
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-11 12:40:29 +0100

    better auto upgrades

--- modules/platform/upgrades.nix
+++ modules/platform/upgrades.nix
@@ -4,6 +4,13 @@
       enable = true;
       flake = "github:Moe1369/nix-fleet#${host}";
       dates = "Sun 04:00";
+      persistent = true;
+    };
+    nix.gc = {
+      automatic = true;
+      dates = "Sun 04:00";
+      options = "--delete-older-than 7d";
+      persistent = true;
     };
   };
 }
