9b892957163f79a7cfc3f2529275d83f4f9cb3bc
Mo <65728018+Moe1369@users.noreply.github.com>
2026-03-12 09:34:59 +0100

    Remove import of unstable Nix packages
    Removed the import of unstable Nix packages from nixsettings.nix.

--- modules/platform/nixsettings.nix
+++ modules/platform/nixsettings.nix
@@ -2,15 +2,9 @@
   systems = [
     "x86_64-linux"
   ];
-
   perSystem = { system, ... }: {
     _module.args.pkgs = import inputs.nixpkgs {
       inherit system;
-      config.allowUnfree = true;
-    };
-
-    _module.args.pkgs-unstable = import inputs.nixpkgs-unstable {
-      inherit system; 
       config.allowUnfree = true;
     };
   };
