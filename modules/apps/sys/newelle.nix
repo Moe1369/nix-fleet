705b5ceae2a6502a92c658b17b156e38df9d2370
Mohamed Chrayed <mohamed@chrayed.de>
2026-03-28 12:17:51 +0100

    wrong name

--- modules/apps/sys/newelle.nix
+++ modules/apps/sys/newelle.nix
@@ -1,5 +1,5 @@
 { ... }: {
-  flake.nixosModules.apps-sys-ollama = { pkgs, ... }: {
+  flake.nixosModules.apps-sys-newelle = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
       newelle
     ];
