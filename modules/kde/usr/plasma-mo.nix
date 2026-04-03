d7d74229880d9daea2937666470545fd3485ea54
Mohamed Chrayed <mohamed@chrayed.de>
2026-04-03 15:45:40 +0200

    plasma user settings

--- modules/kde/usr/plasma-mo.nix
+++ modules/kde/usr/plasma-mo.nix
@@ -19,7 +19,7 @@
     imports = [ inputs.plasma-manager.homeModules.plasma-manager ];
     home.file."Code/.directory".source = ./dotfiles/code-folder;
     home.file.".config/haruna/haruna.conf".source = ./dotfiles/haruna-config;
-    home.file.".local/share/user-places.xbel".source = ./dotfiles/userplaces-mo;
+    home.file.".local/share/user-places.xbel".source = ./dotfiles/userplaces-computer-mo;
     home.file.".config/kde-material-you-colors/config.conf".source = ./dotfiles/kmyc-config;
     home.file.".config/autostart/kde-material-you-colors.desktop".source = ./dotfiles/kmyc-desktop;
     home.file.wallpaper = {
