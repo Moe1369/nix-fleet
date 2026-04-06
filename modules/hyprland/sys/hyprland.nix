85b1242fbfcfd1c41d71ad80bbb71659fa57fb55
Mohamed Chrayed <mohamed@chrayed.de>
2026-04-06 16:15:58 +0200

    add greetd

--- modules/hyprland/sys/hyprland.nix
+++ modules/hyprland/sys/hyprland.nix
@@ -1,9 +1,27 @@
 { inputs, ... }: {
-  flake.nixosModules.hyprland-sys-hyprland = { pkgs, ... }: {
+  flake.nixosModules.hyprland-sys-hyprland = { pkgs, ... }: let
+    tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+    session   = "${pkgs.hyprland}/bin/Hyprland";
+    username  = "mo";
+  in {
     programs.hyprland = {
       enable = true;
       xwayland.enable = true;
       withUWSM = true;
+    };
+
+    services.greetd = {
+      enable = true;
+      settings = {
+        initial_session = {
+          command = session;
+          user    = username;
+        };
+        default_session = {
+          command = "${tuigreet} --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time --cmd ${session}";
+          user    = "greeter";
+        };
+      };
     };
 
     environment.systemPackages = [
