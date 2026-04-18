8ed0b472798b1ae82326193a5aca46a5561c7907
Mohamed Chrayed <mohamed@chrayed.de>
2026-04-18 23:46:31 +0200

    new models

--- modules/apps/sys/ai.nix
+++ modules/apps/sys/ai.nix
@@ -6,7 +6,7 @@
     services.ollama = {
       enable = true;
       package = pkgs.ollama-rocm;
-      loadModels = [ "gpt-oss:20b" ];
+      loadModels = [ "gpt-oss:20b" "gemma4:26b" "gemma4:e4b" ];
     };
   };
 }
