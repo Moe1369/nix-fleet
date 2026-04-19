3c3c9311e28ee7e28e5a357e7648f910c86f9b8c
Mohamed Chrayed <mohamed@chrayed.de>
2026-04-19 22:08:48 +0200

    wyoming

--- modules/apps/sys/ai.nix
+++ modules/apps/sys/ai.nix
@@ -6,12 +6,23 @@
       newelle
       vscode
     ];
+
     services.ollama = {
       enable = true;
       package = pkgs.ollama-rocm;
       host = "0.0.0.0";
       openFirewall = true;
       loadModels = [ "qwen3.6:35b" "gpt-oss:20b" "gemma4:26b" "gemma4:e4b" ];
+    };
+
+    services.wyoming.faster-whisper = {
+      servers.default = {
+        enable = true;
+        model = "large-v3";
+        language = "de";         # oder "auto" für Spracherkennung
+        uri = "tcp://0.0.0.0:10300";
+        device = "cuda";         # ROCm wird als "cuda" angesprochen
+      };
     };
   };
 }
