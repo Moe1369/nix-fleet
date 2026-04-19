d403ed2cf5cb9cf6005af72fbac0e2fd6b4a0de3
Mohamed Chrayed <mohamed@chrayed.de>
2026-04-19 18:17:08 +0200

    string instead of list

--- modules/apps/sys/ai.nix
+++ modules/apps/sys/ai.nix
@@ -9,7 +9,7 @@
     services.ollama = {
       enable = true;
       package = pkgs.ollama-rocm;
-      host = [ "0.0.0.0" ];
+      host = "0.0.0.0";
       loadModels = [ "qwen3.6:35b" "gpt-oss:20b" "gemma4:26b" "gemma4:e4b" ];
     };
   };
