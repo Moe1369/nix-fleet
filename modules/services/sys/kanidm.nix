644b355900bb49f70a74d4b879ef91fa787b40b7
Mohamed Chrayed <mohamed@chrayed.de>
2026-05-31 14:49:56 +0200

    pkgs name

--- modules/services/sys/kanidm.nix
+++ modules/services/sys/kanidm.nix
@@ -22,7 +22,7 @@
     '';
 
     services.kanidm = {
-      package = pkgs.kanidm.withSecretProvisioning_1_10;
+      package = pkgs.kanidmwithSecretProvisioning_1_10;
       server.enable = true;
       server.settings = {
         origin = "https://auth.chrayed.de";
