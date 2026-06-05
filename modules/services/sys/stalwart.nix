0baab8aadee0cf0f9478140e352014af649dffe5
Mohamed Chrayed <mohamed@chrayed.de>
2026-06-05 12:48:07 +0200

    dont use systemd service

--- modules/services/sys/stalwart.nix
+++ modules/services/sys/stalwart.nix
@@ -6,9 +6,9 @@
     sops.secrets."services/stalwart/oauth-secret" = {
       owner = "stalwart-mail";
     };
-    systemd.services.stalwart.environment = {
-      STALWART_PUBLIC_URL = "https://mailadmin.chrayed.de";
-    };
+    #systemd.services.stalwart.environment = {
+    #  STALWART_PUBLIC_URL = "https://mailadmin.chrayed.de";
+    #};
     services.stalwart = {
       stateVersion = "2.0";
       enable = true;
@@ -16,7 +16,7 @@
         "admin" = "%{file:${config.sops.secrets."services/stalwart/adminpass".path}}%";
       };
       settings = {
-        server.hostname = "https://mailadmin.chrayed.de";
+        server.hostname = "mailadmin.chrayed.de";
         server.http.allowed-hosts = [ "mailadmin.chrayed.de" ];
         server.http.cors.allowed-origins = [ "https://mail.chrayed.de" ];
         server.http.permissive-cors = true;
