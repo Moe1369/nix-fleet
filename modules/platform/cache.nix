3cc4458bce17adc7c6970e19509a0d46d242d83f
Mohamed Chrayed <mohamed@chrayed.de>
2026-06-06 11:40:09 +0200

    nixbuild instead of garnix

--- modules/platform/cache.nix
+++ modules/platform/cache.nix
@@ -4,10 +4,14 @@
       path = "/etc/nix/netrc";
       mode = "0400";
     };
+#    nix.settings = {
+#      substituters = [ "https://cache.garnix.io" ];
+#      trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+#      netrc-file = "/etc/nix/netrc";
+#    };
     nix.settings = {
-      substituters = [ "https://cache.garnix.io" ];
-      trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
-      netrc-file = "/etc/nix/netrc";
+      substituters = [ "ssh://eu.nixbuild.net" ];
+      trusted-public-keys = [ "nixbuild.net/4HVEBQ-1:SmBTIoPfsqu9rIAyXcg90ZTkytGdxq2iOHVgjfXn+A8=" ];
     };
     nix.extraOptions = ''
       narinfo-cache-positive-ttl = 3600
