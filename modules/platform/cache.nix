67b1b9c1e4d90d87f7cff5e69771b040fa88494c
Mohamed Chrayed <mohamed@chrayed.de>
2026-06-06 11:56:02 +0200

    nixbuild

--- modules/platform/cache.nix
+++ modules/platform/cache.nix
@@ -1,20 +1,49 @@
 { ... }: {
   flake.nixosModules.platform-sys-cache = { config, ... }: {
-    sops.secrets."garnix/token" = {
-      path = "/etc/nix/netrc";
+    sops.secrets."ssh/nixbuild/public" = {
+      path = "/root/.ssh/nixbuild.pub";
       mode = "0400";
+      owner = "root";
     };
-#    nix.settings = {
-#      substituters = [ "https://cache.garnix.io" ];
-#      trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
-#      netrc-file = "/etc/nix/netrc";
-#    };
-    nix.settings = {
-      substituters = [ "ssh://eu.nixbuild.net" ];
-      trusted-public-keys = [ "nixbuild.net/4HVEBQ-1:SmBTIoPfsqu9rIAyXcg90ZTkytGdxq2iOHVgjfXn+A8=" ];
+    sops.secrets."ssh/nixbuild/private" = {
+      path = "/root/.ssh/nixbuild";
+      mode = "0400";
+      owner = "root";
     };
-    nix.extraOptions = ''
-      narinfo-cache-positive-ttl = 3600
+    programs.ssh.extraConfig = ''
+      Host eu.nixbuild.net
+        IdentityFile /root/.ssh/nixbuild
+        PubkeyAcceptedKeyTypes ssh-ed25519
+        ServerAliveInterval 60
+        IPQoS throughput
     '';
+
+    programs.ssh.knownHosts.nixbuild = {
+      hostNames = [ "eu.nixbuild.net" ];
+      publicKeyFile = config.sops.secrets."ssh/nixbuild/public".path;
+    };
+
+    nix = {
+      distributedBuilds = true;
+      buildMachines = [
+        {
+          hostName = "eu.nixbuild.net";
+          system = "x86_64-linux";
+          maxJobs = 100;
+          supportedFeatures = [ "benchmark" "big-parallel" ];
+        }
+      ];
+      settings = {
+        substituters = [
+          "ssh://eu.nixbuild.net"
+        ];
+        trusted-public-keys = [
+          "nixbuild.net/4HVEBQ-1:SmBTIoPfsqu9rIAyXcg90ZTkytGdxq2iOHVgjfXn+A8="
+        ];
+      };
+      extraOptions = ''
+        narinfo-cache-positive-ttl = 3600
+      '';
+    };
   };
 }
