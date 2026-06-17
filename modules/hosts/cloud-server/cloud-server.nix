9f0d095e303720f2afd52867bfa52f73940ac8c3
Mohamed Chrayed <mohamed@chrayed.de>
2026-06-17 18:00:17 +0200

    add vaultwarden

--- modules/hosts/cloud-server/cloud-server.nix
+++ modules/hosts/cloud-server/cloud-server.nix
@@ -15,6 +15,7 @@
       services-sys-bulwark
       services-sys-caddy
       services-sys-stalwart
+      services-sys-vaultwarden
       users-sys-administrator
     ];
     
---
+++ modules/services/sys/vaultwarden.nix
@@ -0,0 +1,21 @@
+{ ... }: {
+  flake.nixosModules.services-sys-vaultwarden = { config, lib, ... }: {
+    virtualisation.oci-containers.containers."vaultwarden" = {
+      image = "vaultwarden/server:1.36.0";
+      environment = {
+        DOMAIN        = "https://vault.chrayed.de";
+        SIGNUPS_ALLOWED = "false";
+        WEBSOCKET_ENABLED = "true";
+      };
+      ports   = [ "127.0.0.1:8080:80" ];
+      volumes = [
+        "vaultwarden-data:/data"
+      ];
+    };
+    services.caddy.virtualHosts."pw.chrayed.de" = {
+      extraConfig = ''
+        reverse_proxy localhost:8080
+      '';
+    };
+  };
+}
