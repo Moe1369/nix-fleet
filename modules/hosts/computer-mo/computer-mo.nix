6eea9fb97c9936116877b14ed103a8f2d7c19b7f
Mohamed Chrayed <mohamed@chrayed.de>
2026-07-01 11:26:50 +0200

    remove bitwarden

--- modules/hosts/computer-mo/computer-mo.nix
+++ modules/hosts/computer-mo/computer-mo.nix
@@ -10,7 +10,7 @@
       apps-sys-ai
       apps-sys-brave
       apps-sys-libreoffice
-      apps-sys-bitwarden
+      #apps-sys-bitwarden
       base-sys-group
       desktop-sys-group
       disks-sys-singledisk-encrypted
